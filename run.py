481bf39fc295ef0df800cca633042cbb6bc46e4f
HyprexDeva <96543076+hyprex-deva@users.noreply.github.com>
2022-08-29 23:50:14 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+import requests
+city = input("City : ")
+key = "9656046022b784a4ee34bd710a357f3e"
+#city = "Kolkata"
+url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key
+r = requests.get(url)
+
+
+import json
+weather_data = json.loads(r.text)
+#print(weather_data)
+#latest_data = weather_data.list[0]
+latest_data = (weather_data["list"][0]["main"])
+print("Temperature : " + str(latest_data["temp"]))
+print("Humidity : " + str(latest_data["humidity"]))
